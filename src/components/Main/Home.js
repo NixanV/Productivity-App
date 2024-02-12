c045f1104529c950c397402254d0ab58567e727b
Nikolay Petrov <nikip1303@gmail.com>
2024-02-12 15:32:17 +0200

    style added to home page

--- src/components/Main/Home.js
+++ src/components/Main/Home.js
@@ -7,10 +7,16 @@ export const Home = () =>{
         <main>
             <section className={styles["first-section"]}>
                 <div className={styles["left-side-first-section"]}>
-                    <video className={styles["video"]}>
+                    <video className={styles["video"]} controls loop autoPlay>
                         <source src={video} type="video/mp4" />
                             there is no video
                     </video>
+                </div>
+                <div className={styles["right-side-first-section"]}>
+                    <h3 className={styles["h3-motivation"]}>
+                        Here we will show you how to manage your time. <br/>You can do all you want and still have a free time.
+                        <br/> Reading, writing, working out everything is possible. Trust us.
+                    </h3>
                 </div>
             </section>
         </main>
