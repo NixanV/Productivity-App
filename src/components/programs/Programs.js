7fb11d835ea7d8a4d63b6807e2e29cf4a22f2684
Nikolay Petrov <nikip1303@gmail.com>
2024-02-22 14:07:37 +0200

    button and image added for first one

--- src/components/programs/Programs.js
+++ src/components/programs/Programs.js
@@ -1,23 +1,29 @@
-import styles from "./programs.module.css"
-
+import styles from "./programs.module.css";
+import image_one from "./images/How-to-schedule-TikTok-posts_Artboard-1-copy.jpg";
 
 export const Programs = () => {
     return(
-        <>
+        <body >
             <h2>
                 Find perfect program for your schedule.
                 Or create your own one.
             </h2>
-            
+
+                <h3>
+                    Schedules for working 9-5
+                </h3>
             <section className={styles["working-section"]}>
                 <div className={styles["working-first-div"]}>
                     <div>
-                        <img />
+                        <img className={styles["image-first-schedule"]} src={image_one}/>
                     </div>
                     <div>
                         <p>
                             first schedule
                         </p>
+                        <button className={styles["button-go-to"]}>
+                            Go to!
+                        </button>
                     </div>
                 </div>
                 <div className={styles["working-second-div"]}>
@@ -42,7 +48,7 @@ export const Programs = () => {
                 </div>
             </section>
             
-        </>
+        </body>
         
     )
 }
