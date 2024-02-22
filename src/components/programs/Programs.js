07d17a79ce6b0c2b1d37d67379741fdfcfeef9c3
Nikolay Petrov <nikip1303@gmail.com>
2024-02-22 13:32:58 +0200

    programs for working people demo creation

--- src/components/programs/Programs.js
+++ src/components/programs/Programs.js
@@ -1,8 +1,48 @@
+import styles from "./programs.module.css"
+
+
 export const Programs = () => {
     return(
-        <h2>
-            Hello
+        <>
+            <h2>
+                Find perfect program for your schedule.
+                Or create your own one.
+            </h2>
             
-        </h2>
+            <section className={styles["working-section"]}>
+                <div className={styles["working-first-div"]}>
+                    <div>
+                        <img />
+                    </div>
+                    <div>
+                        <p>
+                            first schedule
+                        </p>
+                    </div>
+                </div>
+                <div className={styles["working-second-div"]}>
+                    <div>
+                        <img />
+                    </div>
+                    <div>
+                        <p>
+                            second schedule
+                        </p>
+                    </div>
+                </div>
+                <div className={styles["working-third-div"]}>
+                    <div>
+                        <img />
+                    </div>
+                    <div>
+                        <p>
+                            third schedule
+                        </p>
+                    </div>
+                </div>
+            </section>
+            
+        </>
+        
     )
 }
