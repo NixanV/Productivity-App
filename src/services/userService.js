bf925a11c4494b037d90425a22ec78633d93eeb4
Nikolay Petrov <nikip1303@gmail.com>
2024-02-24 18:37:50 +0200

    register service created

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,3 +8,14 @@ export const login = (userData) => {
     })
     .then(res => res.json())
 }
+
+export const register = (userData) => {
+    return fetch("https://localhost:3030/users/register",{
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body:JSON.stringify(userData)
+    })
+    .then(res => res.json())
+}
