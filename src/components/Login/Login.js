9991ecefef297241cd3d45bad6011073e0719ff4
Nikolay Petrov <nikip1303@gmail.com>
2024-02-24 19:48:06 +0200

    onChamgeHandler created and implemented

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,20 @@
+import { useState } from "react"
 import styles from "./login.module.css"
 
 
 export const Login = () =>{
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const onChangeHandler = (ev) => {
+        setData(state => ({
+            ...state,
+            [ev.target.name] : ev.target.value
+        }));
+    };
+
     return(
     <div className={styles["container"]}>
         <div className={styles["card"]}>
@@ -12,16 +25,25 @@ export const Login = () =>{
                 </h2>
             </div>
             <form className={styles["card-form"]}>
+            
                 <div className={styles["input"]}>
-                    <input type="text" className={styles["input-field"]} value="" required/>
-                    <label className={styles["input-label"]}>Full name</label>
-                </div>
-                            <div className={styles["input"]}>
-                    <input type="text" className={styles["input-field"]} value="" required/>
+                    <input
+                    type="email" 
+                    className={styles["input-field"]}
+                    name="email"
+                    value={data.email}
+                    onChange={(ev) => onChangeHandler(ev)}
+                    required/>
                     <label className={styles["input-label"]}>Email</label>
                 </div>
-                            <div className={styles["input"]}>
-                    <input type="password" className={styles["input-field"]} required/>
+                <div className={styles["input"]}>
+                    <input 
+                    type="password" 
+                    className={styles["input-field"]} 
+                    name="password"
+                    value={data.email}
+                    onChange={(ev) => onChangeHandler(ev)}
+                    required/>
                     <label className={styles["input-label"]}>Password</label>
                 </div>
                 <div className={styles["action"]}>
