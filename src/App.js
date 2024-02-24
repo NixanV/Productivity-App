fe1a3934705b81502cce84d1093fb572b0cfca14
Nikolay Petrov <nikip1303@gmail.com>
2024-02-24 18:47:17 +0200

    Login.js added in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import {Header} from "./components/common/Header/Header";
 import {Home} from "./components/Main/Home";
 import {Programs} from "./components/programs/Programs"
+import { Login } from "./components/Login/Login";
 
 function App() {
   	return (
@@ -11,6 +12,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/programs" element={<Programs />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
     	</div>
   	);
